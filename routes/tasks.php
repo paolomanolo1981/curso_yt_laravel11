225609c9dcd1eba74e9c9b6fe1e4fa4013a4267c
paolomanolo1981 <paolo.manolo28@gmail.com>
2024-10-16 01:51:16 -0500

    kkm

--- routes/tasks.php
+++ routes/tasks.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/task', function () {
     return 'ok';
 });
-//mensaje fg en el comentario afadf mi cambio
+//mensaje fg en el comentario afadf mi cambio para mi cambio
+//esto es nuevo
